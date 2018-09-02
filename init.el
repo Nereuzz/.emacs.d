3f36aff97ed387dadfda11ea60a26cf3493a0a8f
Nereuzz <thomsenpeter1995@hotmail.com>
2018-09-02 22:17:08 +0200

    fresh before format

--- init.el
+++ init.el
@@ -91,12 +91,24 @@
 ;; undo-tree mode
 (global-undo-tree-mode)
 (put 'set-goal-column 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+ '(package-selected-packages
+   (quote
+    (exec-path-from-shell auctex-latexmk yasnippet undo-tree spacemacs-theme solarized-theme neotree marmalade magit graphviz-dot-mode gist fsharp-mode emojify autopair auto-complete auctex))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
-;;latexmk fix
-(exec-path-from-shell-initialize)
-(getenv "PATH")
- (setenv "PATH"
-(concat
-"/Library/TeX/texbin/latex" ":"
 
-(getenv "PATH")))
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
